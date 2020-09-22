7bcb8e1f7b0d5dcdd05939f26071672f2bae8a54
ludo77100 <ludovic.s@protonmail.com>
2020-09-22 10:27:38 +0200

    Ajout de l'initialisation et du reset de l'objet EcritureComptable avec les annotations jUnit @Before et @After

--- myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
+++ myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -3,6 +3,8 @@ package com.dummy.myerp.business.impl.manager;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
@@ -16,21 +18,37 @@ public class ComptabiliteManagerImplTest {
 
     private ComptabiliteManagerImpl manager = new ComptabiliteManagerImpl();
 
+    private EcritureComptable vEcritureComptable ;
 
-    @Test
-    public void checkEcritureComptableUnit() throws Exception {
-        EcritureComptable vEcritureComptable;
+    @Before
+    public void initCompatibiliteManagerImpl(){
         vEcritureComptable = new EcritureComptable();
         vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
         vEcritureComptable.setDate(new Date());
+        vEcritureComptable.setReference("AC-2020/00001");
         vEcritureComptable.setLibelle("Libelle");
         vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
-                                                                                 null, new BigDecimal(123),
-                                                                                 null));
+                null, new BigDecimal(123),
+                null));
         vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(2),
-                                                                                 null, null,
-                                                                                 new BigDecimal(123)));
+                null, null,
+                new BigDecimal(123)));
+    }
+
+    /**
+     * After each test reset the variable vEcritureComptable
+     */
+    @After
+    public void ResetvEcritureComptable(){
+        vEcritureComptable=new EcritureComptable();
+    }
+
+
+    @Test
+    public void checkEcritureComptableUnit() throws Exception {
+
         manager.checkEcritureComptableUnit(vEcritureComptable);
+        Assertions.assertDoesNotThrow(() -> {manager.checkEcritureComptableUnit(vEcritureComptable);});
     }
 
     @Test(expected = FunctionalException.class)
@@ -64,22 +82,16 @@ public class ComptabiliteManagerImplTest {
         vEcritureComptable.setDate(new Date());
         vEcritureComptable.setLibelle("Libelle");
         vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
-                                                                                 null, new BigDecimal(123),
-                                                                                 null));
+                null, new BigDecimal(123),
+                null));
         vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
-                                                                                 null, new BigDecimal(123),
-                                                                                 null));
+                null, new BigDecimal(123),
+                null));
         manager.checkEcritureComptableUnit(vEcritureComptable);
     }
 
     @Test
     public void checkEcritureComptableUnitRG5isOk() throws Exception {
-        EcritureComptable vEcritureComptable ;
-        vEcritureComptable = new EcritureComptable();
-        vEcritureComptable.setReference("AC-2020/00001");
-        vEcritureComptable.setId(1);
-        vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
-        vEcritureComptable.setDate(new Date());
 
         manager.checkEcritureComptableUnitRG5(vEcritureComptable);
         Assertions.assertDoesNotThrow(() -> {manager.checkEcritureComptableUnitRG5(vEcritureComptable);});
