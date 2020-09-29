ca84e04faa12161351782573d5c13a345a12fa86
ludo77100 <ludovic.s@protonmail.com>
2020-09-29 11:14:31 +0200

    Ajout de la connexion à la base de données

---
+++ myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteManagerImplintegrationTest.java
@@ -0,0 +1,13 @@
+package com.dummy.myerp.testbusiness.business;
+
+import org.junit.runner.RunWith;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:com/dummy/myerp/business/bootstrapContext.xml"})
+public class ComptabiliteManagerImplintegrationTest extends BusinessTestCase{
+
+}
